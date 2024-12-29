f49de1d851105415dbe6f5ff8174d71492f99510
Deekshitha <deekshithagowda926@gmail.com>
2024-12-29 18:18:50 +0530

    Initial Commit

---
+++ SwiftuiCrypto/SwiftuiCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiCryptoApp.swift
+//  SwiftuiCrypto
+//
+//  Created by ATEU on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftuiCryptoTests/SwiftuiCryptoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiCryptoTests.swift
+//  SwiftuiCryptoTests
+//
+//  Created by ATEU on 29/12/24.
+//
+
+import Testing
+@testable import SwiftuiCrypto
+
+struct SwiftuiCryptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
